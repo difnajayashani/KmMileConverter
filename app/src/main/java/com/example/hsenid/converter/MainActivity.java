e422967ddac522eff73f840e60a8afc78690c878
difnajayashani <difna4@gmail.com>
2017-07-28 08:49:33 +0530

    working code committed

--- app/src/main/java/com/example/hsenid/converter/MainActivity.java
+++ app/src/main/java/com/example/hsenid/converter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hsenid.converter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,9 +13,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText kmText = (EditText) findViewById(R.id.inputNumber);
-        final EditText mileText = (EditText) findViewById(R.id.outputNumber);
-        Button kmToMileBtn = (Button) findViewById(R.id.buttonKM);
-        Button mileToKmBtn = (Button) findViewById(R.id.buttonMiles);
+        final EditText input = (EditText) findViewById(R.id.inputNumber);
+        final EditText output = (EditText) findViewById(R.id.outputNumber);
+        Button miles2km = (Button) findViewById(R.id.miles2km);
+        Button km2miles = (Button) findViewById(R.id.km2miles);
+
+
+        km2miles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String km = input.getText().toString();
+                if (!km.matches("")){
+                    float kmCount = Float.parseFloat(km);
+                    output.setText( Double.toString((kmCount* 0.621371)));
+                }
+            }
+        });
+
+        miles2km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String miles = input.getText().toString();
+                if (!miles.matches("")){
+                    float mileCount = Float.parseFloat(miles);
+                    output.setText( Double.toString((mileCount* 1.60934)));
+                }
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
